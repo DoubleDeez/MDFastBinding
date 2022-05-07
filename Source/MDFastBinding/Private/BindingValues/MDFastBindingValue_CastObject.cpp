89ad86190a9532956e058ab10d144a711ee336ce
Dylan Dumesnil <dadumesnil@gmail.com>
2022-05-07 15:35:56 -0700

    Add a node to cast objects

---
+++ Source/MDFastBinding/Private/BindingValues/MDFastBindingValue_CastObject.cpp
@@ -0,0 +1,68 @@
+﻿#include "BindingValues/MDFastBindingValue_CastObject.h"
+
+namespace MDFastBindingValue_CastObject_Private
+{
+	static const FName ObjectName = TEXT("Object");
+}
+
+const FProperty* UMDFastBindingValue_CastObject::GetOutputProperty()
+{
+#if !WITH_EDITOR
+	if (ObjectProp == nullptr)
+#endif
+	{
+		if (FProperty* Prop = GetClass()->FindPropertyByName(GET_MEMBER_NAME_CHECKED(UMDFastBindingValue_CastObject, ResultObject)))
+		{
+			if (FObjectPropertyBase* ObjProp = CastField<FObjectPropertyBase>(Prop))
+			{
+				// This is how we make our output dynamic
+				ObjProp->PropertyClass = ObjectClass;
+				ObjectProp = ObjProp;
+			}
+		}
+	}
+	
+	return ObjectProp;
+}
+
+#if WITH_EDITORONLY_DATA
+FText UMDFastBindingValue_CastObject::GetDisplayName()
+{
+	if (ObjectClass != nullptr)
+	{
+		return ObjectClass->GetDisplayNameText();
+	}
+	
+	return Super::GetDisplayName();
+}
+
+TTuple<const FProperty*, void*> UMDFastBindingValue_CastObject::GetValue_Internal(UObject* SourceObject)
+{
+	bool bDidUpdate = false;
+	const TTuple<const FProperty*, void*> ObjectValue = GetBindingItemValue(SourceObject, MDFastBindingValue_CastObject_Private::ObjectName, bDidUpdate);
+	
+	if (bDidUpdate && ObjectValue.Value != nullptr && ObjectClass != nullptr)
+	{
+		ResultObject = nullptr;
+		
+		if (UObject* Object = *static_cast<UObject**>(ObjectValue.Value))
+		{
+			if (Object->IsA(ObjectClass))
+			{
+				ResultObject = Object;
+			}
+		}
+	}
+
+	return { GetOutputProperty(), &ResultObject };
+}
+
+void UMDFastBindingValue_CastObject::SetupBindingItems()
+{
+	Super::SetupBindingItems();
+
+	EnsureBindingItemExists(MDFastBindingValue_CastObject_Private::ObjectName
+			, GetClass()->FindPropertyByName(GET_MEMBER_NAME_CHECKED(UMDFastBindingValue_CastObject, ResultObject))
+			, NSLOCTEXT("MDFastBindingValue_CastObject", "ObjectToolTip", "The object to cast to the selected class."));
+}
+#endif
---
+++ Source/MDFastBinding/Public/BindingValues/MDFastBindingValue_CastObject.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "MDFastBindingValueBase.h"
+#include "MDFastBindingValue_CastObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS(meta = (DisplayName = "Cast Object"))
+class MDFASTBINDING_API UMDFastBindingValue_CastObject : public UMDFastBindingValueBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual const FProperty* GetOutputProperty() override;
+
+#if WITH_EDITORONLY_DATA
+	virtual FText GetDisplayName() override;
+#endif
+
+protected:
+	virtual TTuple<const FProperty*, void*> GetValue_Internal(UObject* SourceObject) override;
+	virtual void SetupBindingItems() override;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Binding", meta = (AllowAbstract))
+	TSubclassOf<UObject> ObjectClass;
+
+private:
+	UPROPERTY(Transient)
+	UObject* ResultObject = nullptr;
+
+	const FProperty* ObjectProp = nullptr;
+};
